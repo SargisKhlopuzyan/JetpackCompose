17b0de2a65746412ac523b89cda5df595f4fa97a
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2022-03-12 17:14:11 +0400

    Creating an Image Card Composable - Android Jetpack Compose - Part 4

--- app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
@@ -3,17 +3,24 @@ package com.sargis.khlopuzyan.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sargis.khlopuzyan.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +28,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            previewContent()
+            PreviewContent()
         }
     }
 }
@@ -30,90 +37,74 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        previewContent()
+        PreviewContent()
     }
 }
 
 @Composable
-fun previewContent() {
-    Column(
-//        horizontalAlignment = Alignment.CenterHorizontally,
+fun PreviewContent() {
+    val painter = painterResource(id = R.drawable.kermit)
+
+    val contextDescription = "Kermit is sitting on the window"
+    val title = "Kermit is sitting on the window"
+
+    Box(
         modifier = Modifier
-            .background(Color.Green)
-            .fillMaxHeight(0.5f)
-            .fillMaxWidth()
-//            .width(600.dp)
-//            .requiredWidth(600.dp)
-//            .padding(top = 50.dp)
-            .border(5.dp, Color.Magenta)
-            .padding(5.dp)
-            .border(5.dp, Color.Blue)
-            .padding(5.dp)
-            .border(10.dp, Color.Red)
-            .padding(10.dp)
-
+            .fillMaxWidth(0.5f)
+            .padding(15.dp)
     ) {
-        Text(
-            text = "Hello123456789",
-            modifier = Modifier
-//                .offset(0.dp, 20.dp)
-                .clickable {
-
-                }
-        )
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-        Text(
-
-            modifier = Modifier
-                .background(color = Color.Red),
-            text = "World"
-        )
+        ImageCard(painter = painter, contextDescription = contextDescription, title = title)
     }
+
 }
 
 @Composable
-fun funRow() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier
-            .background(color = Color.Green)
-            .fillMaxWidth()
-            .fillMaxHeight(0.5f)
-//            .fillMaxSize(1.0f)
+fun ImageCard(
+    painter: Painter,
+    contextDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
     ) {
-        Text(text = "AAA")
-        Text(text = "BBB")
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contextDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
 
-        Text(text = "CCC")
-        Text(text = "DDD")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            ) {
 
-        Text(text = "EEE")
-        Text(text = "FFF")
-    }
-}
+            }
 
-@Composable
-fun funColumn() {
-    Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .background(color = Color.Blue)
-            .fillMaxHeight(1.0f)
-            .fillMaxWidth()
-//            .fillMaxHeight()
-//            .fillMaxSize(0.5f)
-    ) {
-        Text(text = "aaa")
-        Text(text = "bbb")
-
-        Text(text = "ccc")
-        Text(text = "ddd")
-
-        Text(text = "e e e")
-        Text(text = "f f f")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
     }
 }

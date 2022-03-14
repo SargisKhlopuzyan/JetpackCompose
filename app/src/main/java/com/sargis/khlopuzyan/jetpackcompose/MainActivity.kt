e905d25c0975d9a01860107d64c22efbe918d7ec
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2022-03-14 18:39:30 +0400

    Side Effects & Effect Handlers - Android Jetpack Compose - Part 10

--- app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
@@ -2,21 +2,17 @@ package com.sargis.khlopuzyan.jetpackcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 import com.sargis.khlopuzyan.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
@@ -24,49 +20,71 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val constraints = ConstraintSet {
-                val greenBox = createRefFor("green_box")
-                val redBox = createRefFor("red_box")
-                val guideline = createGuidelineFromTop(0.5f)
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
 
-                constrain(greenBox) {
-//                    top.linkTo(parent.top)
-                    top.linkTo(guideline)
-                    start.linkTo(parent.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
+            Scaffold(scaffoldState = scaffoldState) {
+
+//                var counter by remember {
+//                    mutableStateOf(0)
+//                }
+
+                var counter = produceState(initialValue = 0) {
+                    delay(3000L)
+                    value = 4
                 }
 
-                constrain(redBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(greenBox.end)
-                    end.linkTo(parent.end)
-//                    width = Dimension.fillToConstraints
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
+                if (counter.value % 5 == 0 && counter.value > 0) {
+//                    scope.launch {
+//                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+//                    }
+
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+                    }
                 }
 
-                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+                Button(onClick = {
+
+                }) {
+                    Text(text = "Click me: ${counter.value}")
+                }
             }
-
-            ConstraintLayout(constraintSet = constraints, modifier = Modifier.fillMaxSize()) {
-                Box(
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .layoutId("green_box")
-                )
-
-                Box(
-                    modifier = Modifier
-                        .background(Color.Red)
-                        .layoutId("red_box")
-                )
-            }
-
         }
-
     }
 }
+
+var i = 0
+
+@Composable
+fun MyComposable(backPressedDispatcher: OnBackPressedDispatcher) {
+
+    SideEffect {
+        i++
+    }
+
+    val callback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher.addCallback(callback)
+        onDispose {
+            callback.remove()
+        }
+    }
+
+    Button(onClick = {
+
+    }) {
+        Text(text = "Click me")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable

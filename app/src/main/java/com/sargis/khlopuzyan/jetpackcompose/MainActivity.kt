11b619a31922283f8f58afa8b22013d0e2cf5c5a
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2022-03-22 16:01:04 +0400

    Modifiers - Android Jetpack Compose - Part 3

--- app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,12 +23,31 @@ class MainActivity : ComponentActivity() {
             Column(
                 modifier = Modifier
                     .background(Color.Green)
-                    .fillMaxHeight(0.7f)
-                    .width(300.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
+                    .fillMaxSize(0.7f)
+                    .padding(16.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Red)
+                    .padding(5.dp),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                Text(text = "Hello")
+                Text(
+                    text = "Hello World",
+                    modifier = Modifier
+                        .border(5.dp, Color.Yellow)
+                        .padding(5.dp)
+                        .offset(20.dp, 20.dp)
+                        .border(10.dp, Color.Gray)
+                        .padding(10.dp)
+                        .clickable {
+                            println("clickable")
+                        }
+                )
+
+                Spacer(modifier = Modifier.height(50.dp))
                 Text(text = "*****World*****")
                 Text(text = "- - - ! - - -")
             }

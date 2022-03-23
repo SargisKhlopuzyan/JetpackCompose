6bd87597753b376a47b623da7ecbabbfb4bc7242
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2022-03-23 11:15:05 +0400

    Creating an Image Card Composable - Android Jetpack Compose - Part 4

--- app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sargis/khlopuzyan/jetpackcompose/MainActivity.kt
@@ -3,57 +3,97 @@ package com.sargis.khlopuzyan.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sargis.khlopuzyan.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxSize(0.7f)
-                    .padding(16.dp)
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(5.dp),
-//                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(
-                    text = "Hello World",
-                    modifier = Modifier
-                        .border(5.dp, Color.Yellow)
-                        .padding(5.dp)
-                        .offset(20.dp, 20.dp)
-                        .border(10.dp, Color.Gray)
-                        .padding(10.dp)
-                        .clickable {
-                            println("clickable")
-                        }
-                )
 
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "*****World*****")
-                Text(text = "- - - ! - - -")
+        setContent {
+            val painter = painterResource(id = R.drawable.kermit)
+            val title = "Kermit in the snow"
+            val description = "Kermit in the snow"
+            val modifier = Modifier
+
+            Box(modifier = Modifier.padding(16.dp)) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title,
+                    modifier = modifier
+                )
             }
         }
     }
+}
 
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(0.5f),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
+        backgroundColor = Color.Green
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 300f
+                        )
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
+
+    }
 }
 
 
